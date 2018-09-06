bbfc660b50acfcb84912416c61daa69e4d58a4b9
Samuel Antwi <cirsam@hotmail.com>
2018-09-06 12:42:45 -0400

    added changes

---
+++ BluereadySearch/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Write your JavaScript code.
+"use strict";
+(function () {
+    $("input[name=post]").click(function () {
+        if ($("#contact_type").val() == "") {
+            alert("You must select a department");
+
+            return false;
+        }
+
+        $.post("/home/EmailMessage", { firstname: $("input[name=firstname]").val(), lastname: $("input[name=lastname]").val(), email: $("input[name=email]").val(), phone: $("input[name=phone]").val(), message: $("textarea[name=message]").val() }, function (data) {
+
+            $("#contactus").html("<h1 style=\"color:green;\">" + data + " we will be in touch within 24 hours.</h1>");
+
+        });
+    });
+})();
